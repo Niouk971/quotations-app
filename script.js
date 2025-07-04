b1dffa42b9a7f20ce06ebcef74ffb1457ca6ebc8
Niouk971 <florian.polter.77@gmail.com>
2025-07-04 12:45:57 +0200

    etapes 4 et 5 en cours

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
 const formulaire = document.getElementById('form');
-const name = document.getElementById('name');
-const bouton = document.getElementById('submit');
+const citation = document.getElementById('citation');
+const nom = document.getElementById('name');
+const button = document.getElementById('submit');
+// const quote = document.getElementsByClassName('quote');
 
-function getTheValue(text, author) {
-    let text = document.getElementsByClassName('text');
-    let author = document.getElementsByClassName('author');
-}
+let text = document.getElementsByClassName('text');
+let author = document.getElementsByClassName('author');
+let quote = document.getElementsByClassName('quote');
 
-console.log(getTheValue(text, author))
+console.log(formulaire);
+console.log(citation);
+console.log(nom);
+console.log(button);
+console.log(quote);
 
-formulaire.addEventListener(submit, () => {
+console.log(text);
+console.log(author);
+
+// function getTheValue(text, author) {
+//     console.log(text);
+//     console.log(author);
+// }
+
+formulaire.addEventListener('input', () => {
     console.log("test")
 })
+
+button.addEventListener('submit', (event) => {
+    event.preventDefault()
+    console.log("test2")
+    event.preventDefault()
+})
+
+function addQuote(citation, nom) {
+    citation = document.createElement('p');
+    citation.innerText = "test"
+    nom = document.createElement('p');
+    nom.innerText = "test2"
+    quote = document.createElement('div');
+    quote.innerText = `${citation} ${nom}`
+}
+
+console.log(addQuote('Citation : "On ne naît pas femme, on le devient".', 'Autrice : Simone de Beauvoir'));
+
