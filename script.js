711cfa1be0e7e53bb99676ee0c20181e1cfe7bb7
Niouk971 <florian.polter.77@gmail.com>
2025-07-04 11:28:50 +0200

    etapes 1 à 3

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const formulaire = document.getElementById('form');
+const name = document.getElementById('name');
+const bouton = document.getElementById('submit');
 
+function getTheValue(text, author) {
+    let text = document.getElementsByClassName('text');
+    let author = document.getElementsByClassName('author');
+}
+
+console.log(getTheValue(text, author))
+
+formulaire.addEventListener(submit, () => {
+    console.log("test")
+})
